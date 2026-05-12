61ce38151c6803d7fa785e0de5be4af6ded25513
= <=>
2026-05-12 17:45:25 -0300

    feat: add unit tests for UserController and UserService

---
+++ src/modules/user/user.service.spec.ts
@@ -0,0 +1,28 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { UserService } from './user.service';
+import { UserRepository } from './user.repository';
+
+describe(UserService, () => {
+  let service: UserService;
+  let repository: UserRepository;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [UserService, UserRepository],
+    }).compile();
+
+    service = module.get<UserService>(UserService);
+    repository = module.get<UserRepository>(UserRepository);
+  });
+
+  it('should be defined', () => {
+    expect(service).toBeDefined();
+    expect(repository).toBeDefined();
+  });
+
+  describe('create', () => {});
+
+  describe('update', () => {});
+
+  describe('findAll', () => {});
+});
